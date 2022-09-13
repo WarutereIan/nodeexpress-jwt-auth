4281a2edc55e9ea4fd6c25bbeb692e324159ffb4
warutereian <nmwanik111@gmail.com>
2022-09-13 19:33:07 +0300

    initial commit from local repo

---
+++ userModels/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const {isEmail} = require('validator')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true,'Please enter an email'], //2nd value in array is error message
+        unique: true,
+        lowercase:true,
+        validate:[isEmail,'Please enter a valid email']
+    },
+    password:{
+        type: String,
+        required: [true,'Please enter a password'],
+        minlength: [6, 'min length is 6 characters']
+    }
+
+    
+})
+
+//fire a function after doc saved to db
+userSchema.post('save', function (doc, next){
+
+
+    next()
+}) 
+
+//fire a function before doc saved to db
+userSchema.pre('save', async function(next){
+    const salt =await bcrypt.genSalt() // generate salt characters
+    this.password = await bcrypt.hash(this.password,salt) //hash using salt 
+
+    next()
+})
+
+//s
+
+userSchema.statics.login = async function(email, password){
+    const user = await this.findOne({email})
+    if(user){
+        const auth = await bcrypt.compare(password, user.password)
+        if(auth){
+            return user
+        }
+        throw Error('incorrect password')
+    }
+    throw Error('incorrect email')
+}
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
